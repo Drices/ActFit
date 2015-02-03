790a38192644c21176642c7a7d3a513ff9cfd7ea
M. Jungmann <maurice.jungmann@gmail.com>
2015-02-03 23:38:19 +0100

    Navigation active scroll

--- ActFit.Web/ActFit.Web/Scripts/Index.js
+++ ActFit.Web/ActFit.Web/Scripts/Index.js
@@ -1,10 +1,8 @@
-﻿
-function scroll_to(clicked_link, nav_height) {
+﻿function scroll_to(clicked_link, nav_height) {
     var element_class = clicked_link.attr('href').replace('#', '.');
     var scroll_to = 0;
     if (element_class != '.top-content') {
         element_class += '-container';
-        //scroll_to = $(element_class).offset().top - nav_height;
         scroll_to = $(element_class).offset().top - nav_height;
     }
     if ($(window).scrollTop() != scroll_to) {
@@ -13,20 +11,43 @@ function scroll_to(clicked_link, nav_height) {
 }
 
 
-jQuery(document).ready(function () {
+$(document).ready(function () {
+    var sections = $('.section'),
+        nav = $('.nav'),
+        nav_height = nav.outerHeight();
 
     /* Navigation */
     $('a.scroll-link').on('click', function (e) {
         e.preventDefault();
-        scroll_to($(this), $('.navbar').outerHeight());
+        scroll_to($(this), nav_height);
     });
+
+
     // toggle "navbar-no-bg" class
     $('.top-content').waypoint(function () {
         $('.navbar').toggleClass('navbar-no-bg');
     }, {
         offset: function () {
-            return -this.element.clientHeight + $('.navbar').outerHeight()
+            return -this.element.clientHeight + nav_height
         }
+    });
+    
+    // toggle "active" class
+    $(window).on('scroll', function () {
+        var cur_pos = $(this).scrollTop();
+
+        sections.each(function () {
+            var top = $(this).offset().top - nav_height,
+                bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find('a').removeClass('active');
+                sections.removeClass('active');
+
+                $(this).addClass('active');
+                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+            }
+        });
     });
 
     /* Background slideshow */
@@ -42,15 +63,15 @@ jQuery(document).ready(function () {
         if (direction === 'down') { $.backstretch('next'); }
         else if (direction === 'up') { $.backstretch('prev'); }
     }, {
-        offset: function() {
+        offset: function () {
             return -this.element.clientHeight + $('.navbar').outerHeight()
-    }
+        }
     });
 
 });
 
 
-jQuery(window).load(function () {
+$(window).load(function () {
     /*	Loader */
     //$(".loader-img").fadeOut();
     //$(".loader").delay(1000).fadeOut("slow");
@@ -59,5 +80,5 @@ jQuery(window).load(function () {
     //$(".modal-body img").attr("style", "width: 100% !important; height: auto !important;");
 });
 
- new WOW().init();
+new WOW().init();
 
